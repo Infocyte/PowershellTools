982f0fedea29e00f677d093cebe399fbb70ab3a5
Chris <chris@infocyte.com>
2017-07-17 16:24:48 -0500

    Improved Error Handling for disconnects to API

--- Import-HuntICLZs.ps1
+++ Import-HuntICLZs.ps1
@@ -42,7 +42,7 @@ if (-NOT (Test-Path $Path)) {
 	Write-Warning "Path does not exist, place your ICLZ files in $Path"
 	return
 } 
-elseif (-NOT (Get-ChildItem -Path $Path -Filter *.iclz)) {
+elseif (-NOT (Get-ChildItem -Recurse -Path $Path -Filter *.iclz)) {
 	Write-Warning "Path does not contain any .ICLZ files"
 	return
 }
@@ -57,9 +57,20 @@ if ($NewToken.id) {
 	Write-Warning "ERROR: Could not get a token from $HuntServer using credentials $($HuntCredential.username)"
 	return
 }
-Start-Sleep 1
 # Error if token no longer valid is:
 # WARNING: Error: The underlying connection was closed: An unexpected error occurred on a send.
+
+#Copy .iclz files into upload folder (temp dir)
+$TempFolderName = "temp$([guid]::NewGuid())"
+Write-Host "Copying folder of .iclz files to staging temp directory: $UploadDir\$TempFolderName"
+Copy-Item -Path $Path -Destination $UploadDir\$TempFolderName -recurse -Container
+<# 
+# TODO: Change this to grab the iclz files only and rename them using their md5 hash so we're not uploading the same iclz file twice (which would break everything)
+Get-ChildItem $Path -filter *.iclz | Foreach-Object { 
+	$newhash = (Get-Hashes -Path $_ -Type MD5).md5
+	Copy-Item -Path $_ -Destination $UploadDir\$TempFolderName\Survey-$newhash.json.iclz -recurse -Container
+}	
+#>
 
 # Get Target Lists.  
 $TargetList = Get-ICTargetList
@@ -91,6 +102,7 @@ if ($TargetList) {
 	Write-Host "Waiting for enumeration to complete"
 	Write-Progress -Activity "Enumerating Target" -status "Initiating Enumeration"
 	while ($active) { 
+		Start-Sleep 1
 		$status = Get-ICActiveTasks
 		if ($status -match "Error") {
 			Write-Host "$Status"
@@ -112,7 +124,6 @@ if ($TargetList) {
 			$lastStatus = $status
 			$elapsedtime = "$($($status.elapsed)/1000)"
 			Write-Progress -Activity "Enumerating Target" -status "[Elapsed (seconds): $elapsedtime] $($status.message)" -percentComplete ($status.progress)	
-			Start-Sleep 0.5
 		} elseif ($Status.message -match "error") {
 			$active = $false
 			Write-Warning "ERROR: Could not enumerate Target: $($Status.message)"
@@ -125,17 +136,6 @@ if ($TargetList) {
 }
 Start-Sleep 1
 
-#Copy .iclz files into upload folder (temp dir)
-$TempFolderName = "temp$([guid]::NewGuid())"
-Write-Host "Copying folder of .iclz files to staging temp directory: $UploadDir\$TempFolderName"
-Copy-Item -Path $Path -Destination $UploadDir\$TempFolderName -recurse -Container
-<# 
-# TODO: Change this to grab the iclz files only and rename them using their md5 hash so we're not uploading the same iclz file twice (which would break everything)
-Get-ChildItem $Path -filter *.iclz | Foreach-Object { 
-	$newhash = (Get-Hashes -Path $_ -Type MD5).md5
-	Copy-Item -Path $_ -Destination $UploadDir\$TempFolderName\Survey-$newhash.json.iclz -recurse -Container
-}	
-#>
 
 Write-Host "Retrieving Last Job and ScanId"
 $LastFolder = (gci 'C:\Program Files\Infocyte\Hunt\uploads\' | Sort-Object LastWriteTime -Descending)[0].Name
@@ -147,16 +147,16 @@ if ($ScanJobs -match "Error") {
 	if ($NewToken.id) {
 		Write-Host "Login successful to $HuntServer"
 		Write-Host "Login Token id: $($NewToken.id)"
+		$ScanJobs = Get-ICActiveJobs
 	} else {
 		Write-Warning "ERROR: Could not get a token from $HuntServer using credentials $($HuntCredential.username)"
 		return
 	}
-} else {
-	$ScanJobs = $ScanJobs | Sort-Object timestamp -Descending | where { $_.status -eq "Scanning" }
-}
+} 
 
+$ScanJobs = $ScanJobs | Sort-Object timestamp -Descending | where { $_.status -eq "Scanning" }
 if ($ScanJobs) {
-	$baseScanId = $ScanJobs[0].ScanId
+	$baseScanId = $ScanJobs[0].scanId
 } else {
 	$baseScanId = "NO_SCAN"
 }
@@ -166,11 +166,28 @@ Write-Host "Last Active ScanId: $baseScanId"
 
 # Initiate Scan
 Write-Host "Initiating Scan of $Target"
-Invoke-ICScan $TargetListId
+$ScanTask = Invoke-ICScan $TargetListId
+Start-Sleep 1
+if ($ScanTask -match "Error") {
+	Write-Warning "$ScanTask"
+	Write-Host "Attempting to re-connecting to $HuntServer"
+	$NewToken = New-ICToken $HuntCredential $HuntServer
+	if ($NewToken.id) {
+		Write-Host "Login successful to $HuntServer"
+		Write-Host "Login Token id: $($NewToken.id)"
+        $ScanTask = Invoke-ICScan $TargetListId
+		Start-Sleep 1
+	} else {
+		Write-Warning "ERROR: Could not get a token from $HuntServer using credentials $($HuntCredential.username)"
+		return
+	}
+}
+
 
 # Wait for new scan to be created
 $scanId = $baseScanId
 while ($scanId -eq $baseScanId) {
+	Start-Sleep 2
 	$ScanJobs = Get-ICActiveJobs
 	if (!$ScanJobs -OR ($ScanJobs -match "Error")) {
 		Write-Warning "$ScanJobs"
@@ -183,18 +200,16 @@ while ($scanId -eq $baseScanId) {
 			Write-Warning "ERROR: Could not get a token from $HuntServer using credentials $($HuntCredential.username)"
 			return
 		}
-	} else {
-		$ScanJobs = $ScanJobs | Sort-Object timestamp -Descending | where { $_.status -eq "Scanning" }
-	}
-	
-	if ($ScanJobs) {
-		$scanId = $ScanJobs[0].ScanId
-		Write-Host "Waiting for new ScanId to be created... ScanID is currently $scanID $(Get-Date)"
+	} elseif ($ScanJobs) {
+        $ScanJobs = $ScanJobs | Sort-Object timestamp -Descending | where { $_.status -eq "Scanning" }
+		if ($ScanJobs) {
+            $scanId = $ScanJobs[0].ScanId
+        }
+		Write-Host "Waiting for new ScanId to be created... ScanID is currently $scanID as of $(Get-Date)"
 	} else {
 		Write-Warning "No Active Scan! Waiting for scan to be initiated..."
 		$ScanId = "NO_SCAN"
 	}
-	Start-Sleep 2
 }
 Write-Host "New ScanID created! Now: $scanId"
 
@@ -202,13 +217,14 @@ Write-Host "Renaming $UploadDir\$TempFolderName Directory to $UploadDir\$ScanId"
 if (Test-Path $UploadDir\$ScanId) {
 	Write-Warning "Folder $UploadDir\$ScanId already exists!"
 } else {
-	Rename-Item $UploadDir\$TempFolderName $UploadDir\$ScanId
+	Rename-Item $UploadDir\$TempFolderName $UploadDir\$ScanId -Force
 }
 Write-Host "Your HostSurvey results will be processed as the current scan of TargetList $TargetListName moves to the processing phase."
 
 # Track Status of Scan processing
 $active = $true
 while ($active) { 
+	Start-Sleep 0.5
 	$status = Get-ICActiveTasks
 	if ($status -match "Error") {
 		Write-Host "$Status"
@@ -217,6 +233,7 @@ while ($active) {
 		if ($NewToken.id) {
 			Write-Host "Login successful to $HuntServer"
 			Write-Host "Login Token id: $($NewToken.id)"
+			continue
 		} else {
 			Write-Warning "ERROR: Could not get a token from $HuntServer using credentials $($HuntCredential.username)"
 			return
