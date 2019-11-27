cac52985f82f312f4ab3c9ca16df8cd83940db10
Chris <chris@infocyte.com>
2019-11-27 12:57:38 -0600

    Enhanced Agent Deployment Workflow (#6)
    * expanded script with additional options
    no breaking changes
    
    * updated readme
    * Added uninstallagent command

--- AgentDeployment/install_huntagent.ps1
+++ AgentDeployment/install_huntagent.ps1
@@ -1,4 +1,4 @@
-New-Module -name install_huntagent -scriptblock {
+New-Module -name install_InfocyteAgent -scriptblock {
 	# Infocyte HUNT scripted installation option. If unfamiliar with this script, contact your IT or Security team.
 	# www.infocyte.com
 
@@ -11,44 +11,72 @@ New-Module -name install_huntagent -scriptblock {
 	# Example:
 	# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex; installagent alpo1 asdfhrendsa
 
-	# Logs are stored here: "$($env:TEMP)\huntagentinstall.log"
+	# Logs are stored here: "C:\Windows\Temp\infocyteagentinstaller.log"
 
-	Function Install-HuntAgent() {
+	Function Install-InfocyteAgent() {
+		<#
+		.SYNOPSIS
+		Installs the Infocyte agent
+
+		.DESCRIPTION
+		Installs the Infocyte agent
+
+		.ALIASES
+		installagent
+
+		.LINK
+		https://github.com/Infocyte/PowershellTools/master/AgentDeployment
+
+		#>
 		param(
-			[Parameter(Position = 0)]
+			[Parameter(Mandatory=$true, Position = 0, HelpMessage="The cname from the URL: https://<instancename>.infocyte.com)")]
 			[String]$InstanceName,
 
-			[Parameter(Position = 1)]
+			[Parameter(Position = 1, HelpMessage="This will automatically approve the agent registration and add it to its' default Target Group.")]
 			[String]$RegKey,
 
-			[Switch]$Interactive
+			[Parameter(HelpMessage="Will register a name for the system. Otherwise will use the hostname.")]
+			[String]$FriendlyName,
+
+			[Parameter(HelpMessage='Authenticated: "user:password@192.168.1.1:8080" or Unauthenticated: "192.168.1.1:8080"')]
+			[String]$Proxy, # "user:password@192.168.1.1:8080" or "192.168.1.1:8080"
+
+			[Parameter(HelpMessage="Silent install is default. Use this switch to display output.")]
+			[Switch]$Interactive,
+
+			[Parameter(HelpMessage="Will force a reinstall if agent already installed.")]
+			[Switch]$Force
 		)
 
 		$agentDestination = "$($env:TEMP)\agent.windows.exe"
-		$LogPath = "$($env:TEMP)\huntagentinstall.log"
+		$LogPath = "$env:SystemDrive\windows\Temp\infocyteagentinstaller.log"
 		$agentURL = "https://s3.us-east-2.amazonaws.com/infocyte-support/executables/agent.windows.exe"
 		$hunturl = "https://$InstanceName.infocyte.com"
 
-		# Make script silent unless run interactive
-		if (-NOT $Interactive) { $ErrorActionPreference = "silentlycontinue" }
-
 		If (-NOT $InstanceName) {
-			Write-Error "Please provide Infocyte HUNT instance name (i.e. mycompany in mycompany.infocyte.com)"
-			"$(Get-Date) [Error] Installation Error: Install started but no InstanceName provided in arguments." >> $LogPath
+			Write-Warning "[Error] Please provide Infocyte HUNT instance name (i.e. mycompany in mycompany.infocyte.com)"
+			"$(Get-Date) [Error] Installation Error: No InstanceName provided in arguments!" >> $LogPath
 			return
 		}
 
 		If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-			Write-Error "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-			"$(Get-Date) [Error] Installation Error: Install started but script not run as administrator" >> $LogPath
+			Write-Warning "[Error] You do not have Administrator rights to run this script!`nPlease re-run as an Administrator!"
+			"$(Get-Date) [Error] Installation Error: Script not run as administrator!" >> $LogPath
 			return
 		}
 
+		# Make script silent unless run interactive
+		if (-NOT $Interactive) { $ErrorActionPreference = "silentlycontinue" }
+
 		$InstallPath = 'C:\Program Files\Infocyte\Agent\agent.windows.exe'
 		If (Get-Service -name huntAgent -ErrorAction SilentlyContinue) {
-			if ($Interactive) { Write-Error "huntAgent service already installed" }
-			"$(Get-Date) [Information] Install started but HUNTAgent service already running. Skipping." >> $LogPath
-			return
+			if ($Force) {
+				$Uninstall = $True
+			} else {
+				if ($Interactive) { Write-Error "Infocyte Agent (HUNTAgent) service already installed" }
+				"$(Get-Date) [Information] Install started but HUNTAgent service already running. Skipping." >> $LogPath
+				return
+			}
 		}
 
 		# Downloading Agent
@@ -60,7 +88,7 @@ New-Module -name install_huntagent -scriptblock {
 		try {
 			$wc.DownloadFile($agentURL, $agentDestination)
 		} catch {
-			if ($Interactive) { Write-Error "Could not download HUNT agent from $agentURL" }
+			if ($Interactive) { Write-Warning "Could not download HUNT agent from $agentURL" }
 			"$(Get-Date) [Error] Installation Error: Install started but could not download agent.windows.exe from $agentURL." >> $LogPath
 		}
 
@@ -76,25 +104,120 @@ New-Module -name install_huntagent -scriptblock {
 			#"$(Get-Date) [Warning] Installation Warning: Could not hash agent.survey.exe." >> $LogPath
 		}
 
+		# Uninstall if already installed and forcing a reinstall
+		if ($Force -AND $Uninstall) {
+			if ($Interactive) { Write-Warning "Agent service already installed. Forcing a reinstall." }
+			"$(Get-Date) [Information] HUNTAgent service already running. Forcing a reinstall." >> $LogPath
+
+			$arguments = @("--uninstall")
+			if (-NOT $interactive) { $arguments += "--quiet" }
+			try {
+				Start-Process -NoNewWindow -FilePath $agentDestination -ArgumentList $arguments -Wait -ErrorAction Stop
+				#& $agentDestination $arguments
+			} catch {
+				if ($Interactive) { Write-Error "$(Get-Date) [Error] Uninstall Error: Could not start agent.windows.exe. [$_]" }
+				"$(Get-Date) [Error] Uninstall Error: Could not start agent.windows.exe. [$_]" >> $LogPath
+				return
+			}
+		}
+
 		# Setup exe arguments
 		#$arguments = "--url $hunturl --install"
-		#if (-NOT $interactive) { $arguments += " --quiet" }
+		#if ($Silent) { $arguments += " --quiet" }
 		#if ($RegKey) { $arguments += " --key $RegKey" }
-
 		$arguments = @("--install", "--url $hunturl")
 		if ($RegKey) { $arguments += "--key $RegKey" }
-		if (-NOT $interactive) { $arguments += "--quiet" }
+		if ($FriendlyName) { $arguments += "--friendly $FriendlyName" }
+		if ($Proxy) { $arguments += "--proxy $Proxy" }
+		if (-NOT $Interactive) { $arguments += "--quiet" }
 
-		"$(Get-Date) [Information] Installing Agent: Downloading agent.windows.exe from $agentURL [sha1: $sha1] and executing with commandline: $($agentDestination.Substring($agentDestination.LastIndexOf('\')+1)) $arguments" >> $LogPath
+		$version = & "$agentDestination" --version
+		if ($version -notmatch "HUNT Agent") {
+			if ($Interactive) { Write-Warning "$(Get-Date) [Error] $agentDestination (version: $version, sha1: $sha1) is not valid or appears to be corrupt." }
+			"$(Get-Date) [Error] $agentDestination (version: $version, sha1: $sha1) is not valid or appears to be corrupt." >> $LogPath
+		}
+
+		if ($Interactive) { Write-Host "$(Get-Date) [Information] Downloaded agent.windows.exe (version: $version, sha1: $sha1) from $agentURL" }
+		if ($Interactive) { Write-Host "$(Get-Date) [Information] Installing Agent: $($agentDestination.Substring($agentDestination.LastIndexOf('\')+1)) $arguments" }
+		"$(Get-Date) [Information] Installing Agent: Downloaded agent.windows.exe from $agentURL [sha1: $sha1] and executing with commandline: $($agentDestination.Substring($agentDestination.LastIndexOf('\')+1)) $arguments" >> $LogPath
 		# Execute!
 		try {
-			Start-Process -NoNewWindow -FilePath $agentDestination -ArgumentList $arguments -ErrorAction Stop
+			Start-Process -NoNewWindow -FilePath $agentDestination -ArgumentList $arguments -Wait -ErrorAction Stop
+			if ($Interactive) { Write-Host "$(Get-Date) [Success] Installation Succeeded! Agent associated to $InstanceName." }
+			"$(Get-Date) [Success] Installation Succeeded! Agent associated to $InstanceName." >> $LogPath
+
 			#& $agentDestination $arguments
 		} catch {
+			if ($Interactive) { Write-Error "$(Get-Date) [Error] Installation Error: Could not start agent.windows.exe. [$_]" }
 			"$(Get-Date) [Error] Installation Error: Could not start agent.windows.exe. [$_]" >> $LogPath
 		}
 
 	}
-Set-Alias installagent -Value Install-HuntAgent | Out-Null
-Export-ModuleMember -Alias 'installagent' -Function 'Install-HuntAgent' | Out-Null
+
+	Function Uninstall-InfocyteAgent() {
+		<#
+		.SYNOPSIS
+		Uninstalls the Infocyte agent
+
+		.DESCRIPTION
+		Uninstalls the Infocyte agent
+
+		.Aliases
+		uninstallagent
+
+		.LINK
+		https://github.com/Infocyte/PowershellTools/master/AgentDeployment
+
+		#>
+		param(
+			[Parameter(HelpMessage="Use this switch silence output.")]
+			[Switch]$Silent
+		)
+		$LogPath = "$env:SystemDrive\windows\Temp\infocyteagentinstaller.log"
+		$AgentPath = 'C:\Program Files\Infocyte\Agent\agent.windows.exe'
+
+		If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+			Write-Warning "[Error] You do not have Administrator rights to run this script!`nPlease re-run as an Administrator!"
+			"$(Get-Date) [Error] Installation Error: Script not run as administrator!" >> $LogPath
+			return
+		}
+
+		# Make script silent unless run interactive
+		if ($Silent) { $ErrorActionPreference = "silentlycontinue" }
+
+		If (Get-Service -name HUNTAgent -ErrorAction SilentlyContinue) {
+			$Installed = $True
+			if (-NOT $Silent) { Write-Host "Uninstalling Infocyte Agent." }
+			"$(Get-Date) [Information] Uninstalling Infocyte Agent." >> $LogPath
+
+			# Uninstall
+			$arguments = @("--uninstall")
+			if ($Silent) { $arguments += "--quiet" }
+
+			try {
+				Start-Process -NoNewWindow -FilePath $AgentPath -ArgumentList $arguments -Wait -ErrorAction Stop
+				#& $agentDestination $arguments
+			} catch {
+				if (-NOT $Silent) { Write-Error "$(Get-Date) [Error] Uninstall Error: Could not execute agent.windows.exe --uninstall. [$_]" }
+				"$(Get-Date) [Error] Uninstall Error: Could not execute agent.windows.exe --uninstall. [$_]" >> $LogPath
+			}
+
+		} else {
+			$proc = Get-Process -name agent.windows
+			if ($proc) {
+				if (-NOT $Silent) { Write-Warning "Agent was not installed but it is running non-persistent mode. Killing process." }
+				"$(Get-Date) [Information] Agent was not installed but is running non-persistent mode. Killed process." >> $LogPath
+				Get-Process -name agent.windows | Stop-Process | Out-Null
+				Remove-Item $proc.Path -Force
+			} else {
+				if (-NOT $Silent) { Write-Warning "Agent was not installed." }
+				"$(Get-Date) [Information] Attempted to uninstall Agent but was not installed. Skipping." >> $LogPathvvv
+			}
+		}
+	}
+
+	Set-Alias installagent -Value Install-InfocyteAgent | Out-Null
+	Set-Alias uninstallagent -Value Uninstall-InfocyteAgent | Out-Null
+	Export-ModuleMember -Alias 'installagent' -Function 'Install-InfocyteAgent' | Out-Null
+	Export-ModuleMember -Alias 'uninstallagent' -Function 'Uninstall-InfocyteAgent' | Out-Null
 }
--- AgentDeployment/readme.md
+++ AgentDeployment/readme.md
@@ -1,28 +1,62 @@
-## Infocyte HUNT Agent One-Line Powershell Installer  
-**Platform:** Windows  
-**Powershell Version:** 3.0+
+## Infocyte Agent Scripted Installer (One Line Powershell)
+**Platform:** Microsoft Windows 7+ or Server 2008R2+\
+**Powershell Version:** 3.0+\
 **.NET Version:** 4.5+
 
-The following command is all you need. Run it on any windows system and it will download this script and execute it. This is useful for scripted software distribution, sccm, or GPO deployments in leue of an MSI. The script will manage the installation process for the HUNT agent.
+The following command is all you need.  Run it on any windows system and it will download this script and execute it.  This is useful for scripted software distribution, sccm, or GPO deployments in leu of an MSI.  The script will manage an automated installation process for the HUNT agent.  *IMPORTANT: You DO NOT need to download this script. Leave it here unless you want to host it yourself.*
 
-IMPORTANT: You DO NOT need to download this script. Leave it here unless you want to host it yourself.
+To execute this script on a windows host, run this command replacing `<instancename>` and `[regkey]` with your hunt instance \<mandatory\> and registration key [optional].
 
-To execute this script as a one liner on a windows host with powershell 2.0+, run this command replacing `instancecname` and `regkey` with your hunt instance <mandatory> and registration key [optional]. NOTE: Instance name is your cname from the URL, not the FULL url https://instancecname.infocyte.com). This script will append the url for you during install.
 
-```[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex; installagent instancecname regkey```
+> `[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://github.com/Infocyte/PowershellTools/blob/agentdeployment-workflow/AgentDeployment/install_huntagent.ps1") | iex; installagent <instancename> [regkey]`
 
-The arguments are after the command *installagent*:  
-**1st Arg [Manadatory]:** `instancecname`  
-**2nd Arg [Optional]:** `regkey`
 
-Example:  
-```[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex; installagent myhuntinstance asdfasdf```
+#### The positional arguments after the command *installagent* are:  
+* **(1st Argument) \<Mandatory\>:** `-InstanceName instancename`
+* **(2nd Argument) [Optional]:** `-RegKey regkey`
 
-If you want to troubleshoot or check out what is happening, run in interactive mode or check the log file:
-Add `-Interactive` to the end of the command.
+Instance Name (*instancename*) is your cname from the URL, not the FULL url https://instancename.infocyte.com).
 
-Log can be read here:
-Get-Content "$($env:TEMP)\huntagentinstall.log"
+Registration Key (*regkey*) is created in the Agent Admin panel. This will automatically approve the agent registration and add it to its' default Target Group. Without it, the agent will initially report as "pending" in the web console and cannot be used until approved.
+
+Note: *InstanceName* (1) and *RegKey* (2) are positional arguments so they don't require argument headers if in position 1 and 2 after `installagent`.
+
+### Example 1 (instancename only):  
+> `[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+(new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex;
+installagent demo1`
+
+### Example 2 (For use in batch or GPO):
+If running from outside Powershell (like in a batch or GPO install script):
+> `powershell.exe -ExecutionPolicy bypass -noprofile -nologo -command { [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex; installagent alpo1 }`
 
 ---
 
+## Additional (Optional) Parameters:
+* *-Interactive*
+* *-Force*
+* *-FriendlyName "String"*
+* *-Proxy "user:password@192.168.1.1:8080"*
+
+
+##### Interactive Mode
+Silent run is default so if you want to troubleshoot or check out what is happening, check the log file or run the command in interactive mode:  Add *-Interactive* to the end of the command.
+
+Log can be read here:
+> `Get-Content "C:\Windows\Temp\infocyteagentinstaller.log"`
+
+##### Proxy Configuration:
+* Authenticated: *-Proxy "\<user\>:\<password\>@\<ProxyAddress\>:\<ProxyPort\>"*
+* Unauthenticated: *-Proxy "\<ProxyAddress\>:\<ProxyPort\>"*
+
+##### Force Reinstall:
+Use *-Force* to force a reinstall (by default it bails)
+
+##### Friendly Name:
+Use *-FriendlyName* to add a descriptive name for the system (otherwise it uses hostname). This can be added or changed in the web console as well after install.
+
+
+### Example 3 (lots of arguments):  
+> `[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+(new-object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1") | iex;
+installagent -InstanceName "demo1" -RegKey "asdfasdf" -FriendlyName "DBServer1" -Proxy "user:password@192.168.1.1:8080" -Interactive`
