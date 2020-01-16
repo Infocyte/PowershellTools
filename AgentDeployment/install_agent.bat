e557ca130d716e577ec8e6a9c1a87a2b84bc503a
Chris <chris@infocyte.com>
2020-01-16 09:07:36 -0600

    now compatible with win2k8 and .net 4.0+

--- AgentDeployment/install_agent.bat
+++ AgentDeployment/install_agent.bat
@@ -2,8 +2,14 @@
 :: Install Infocyte Agent
 :: For use in a GPO Startup Script (Note: Logon script will not work as it operates with the user's non-admin permissions)
 :: Best Reference for steps: https://www.petri.com/run-startup-script-batch-file-with-administrative-privileges
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -nologo -win 1 -executionpolicy bypass -nop -command "& { [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1') | iex; installagent myhuntinstance myregkey }"
-:: Change "myhuntinstance" to your cname
-:: Change "myregkey" to your registration key made in the Infocyte HUNT admin panel (or leave blank if not using)
-:: for testing, you can add a -interactive to the installagent command. The end of the command would look like this:
-:: ...installagent myhuntinstance myregkey -interactive }"
+
+:: Change "instancename" to your cname
+:: Change "regkey" to your registration key made in the Infocyte HUNT admin panel (or leave blank if not using)
+
+set instancename=demo1 
+set regkey=
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -nologo -win 1 -executionpolicy bypass -nop -command "& { [System.Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([System.Net.SecurityProtocolType], 3072); (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Infocyte/PowershellTools/master/AgentDeployment/install_huntagent.ps1') | iex; installagent %instancename% %regkey% }"
+
+:: for testing, you can add a -interactive to the installagent command. The end of the above command would look like this:
+:: ...installagent %instancename% %regkey% -interactive }"
